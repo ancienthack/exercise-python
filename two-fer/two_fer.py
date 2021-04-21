a1184d26294f969e34be0519de2a8251a4a9618c
ancient <60585287+ancienthack@users.noreply.github.com>
2021-04-20 22:48:22 -0600

    initialization. early python exercises.

---
+++ two-fer/two_fer.py
@@ -0,0 +1,8 @@
+def two_fer(name=''):
+    if name != '':
+        return 'One for ' + name + ', one for me.'
+    else:
+        return 'One for you, one for me.'
+
+print(two_fer())
+print(two_fer('Travis'))
